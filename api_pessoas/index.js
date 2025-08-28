a34b65b4c19df3af47d55b932e4ebd6f7a8bcc62
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-08-27 22:14:38 -0300

    projeto api_pessoas

---
+++ api_pessoas/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const pessoas = [];
+
+// Rota para cadastro de pessoas
+pessoas.push();
+// Rota para consulta de pessoas
+pessoas
+// Rota para consultar pessa por nome
+const retorno = pessoas.find(()=> { i.nome === nome});
+
+// Rota para deletar pessoa
+
+
+app.listen(8080,()=>{
+    console.log("O servidor está rodando na porta 8080");
+});
