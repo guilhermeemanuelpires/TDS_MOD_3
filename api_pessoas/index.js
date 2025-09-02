908d5bfb3ae3d908d4a39c9b81a063a93a628248
Guilherme Pires <guilhermeemanuel45@gmail.com>
2025-09-02 20:18:01 -0300

    api pessoas

--- api_pessoas/index.js
+++ api_pessoas/index.js
@@ -2,18 +2,79 @@ const express = require("express");
 
 const app = express();
 
-const pessoas = [];
+app.use(express.json());
 
-// Rota para cadastro de pessoas
-pessoas.push();
-// Rota para consulta de pessoas
-pessoas
-// Rota para consultar pessa por nome
-const retorno = pessoas.find(()=> { i.nome === nome});
+const pessoas = [
+    { id: 1, nome: "Guilherme", telefone: "465999756350" }
+];
 
-// Rota para deletar pessoa
+var contador = 1;
 
-
-app.listen(8080,()=>{
-    console.log("O servidor está rodando na porta 8080");
+//CONSULTAR TODOS AS PESSOAS
+app.get("/pessoas", (request, response) => {
+    return response.send(pessoas);
 });
+
+//CADASTRAR NOVAS PESSOAS
+app.post("/pessoas", (request, response) => {
+    const nome = request.body.nome;
+    const telefone = request.body.telefone;
+    const status = request.body.status;
+
+    contador++;
+
+    pessoas.push({
+        id: contador,
+        nome,
+        telefone,
+        status
+    });
+
+    return response.send(pessoas);
+});
+
+//FILTRA PESSOA PELO NOME 
+app.get("/pessoas/:id", (request, response) => {
+    const id = request.params.id;
+
+    const pessoa = pessoas.filter(i => i.id == id);
+
+    if (pessoa.length <= 0) {
+        return response.send({
+            mensagem: "Nenhuma pessoa encontrada com esse id!"
+        });
+    }
+
+    return response.send(pessoa);
+});
+
+app.put("/pessoas", (request, response) => {
+
+    const { id, nome, telefone, status } = request.body;
+
+    if (id == undefined) {
+        return response.send({
+            mensagem:
+                "O campo id é obrigatorio no corpo da requisição!"
+        });
+    }
+
+    const pessoa = pessoas.filter(item => item.id == id);
+
+    if (pessoa.length <= 0 ){
+        return response.send({mensagem: "Código da pessoa inválido!"});
+    }
+
+    return response.send(pessoa);
+
+});
+
+app.listen(8080, () => {
+    console.log("o servidor está rodando na porta 8080.");
+});
+
+//ATUALIZAR UMA PESSOA JÁ EXISTENTE
+// app.put();
+
+//DELETAR UMA PESSOA JÁ EXISTENTE
+// app.delete();
